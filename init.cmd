51d3686ae1743b664836287c890c542870f15325
Dan Budiac <dansays@gmail.com>
2015-07-29 14:30:33 -0400

    Add bashrc and init.cmd for Windows compatibility

---
+++ init.cmd
@@ -0,0 +1,3 @@
+@echo off
+mklink /H %USERPROFILE%\.bashrc %USERPROFILE%\Documents\Projects\dotfiles\dotfiles\bashrc
+mklink /H %USERPROFILE%\.gitconfig %USERPROFILE%\Documents\Projects\dotfiles\dotfiles\gitconfig
